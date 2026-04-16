dd0ea1ea60f129bbb88c9d9f79c0ceac12c67872
unknown <23ai009@sitmng.ac.in>
2026-04-16 20:41:25 +0530

    Add the create.sql code

---
+++ day2/crud operation basics/create.sql
@@ -0,0 +1,11 @@
+CREATE Table student(
+    id int PRIMARY KEY,
+    name varchar(50),
+    age int,
+    city VARCHAR(50)
+)
+
+INSERT INTO student(id,name,age,city) VALUES(1,'John',20,'New York');
+INSERT INTO student(id,name,age,city) VALUES(2,'Alice',22,'Los Angeles');
+INSERT INTO student(id,name,age,city) VALUES(3,'Bob',19,'Chicago');
+INSERT INTO student(id,name,age,city) VALUES(4,'Eve',21,'Houston');

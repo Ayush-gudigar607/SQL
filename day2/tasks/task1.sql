ca5ab926683f46d29a920fa2755b42371ce9b729
unknown <23ai009@sitmng.ac.in>
2026-04-17 22:21:11 +0530

    add the task 1

---
+++ day2/tasks/task1.sql
@@ -0,0 +1,6 @@
+-- Insert 5 new students
+insert into students(id,name,age,email) VALUES(1,'John',20,'john@example.com');
+insert into students(id,name,age,email) VALUES(2,'Alice',22,'alice@example');
+insert into students(id,name,age,email) VALUES(3,'Bob',19,'bro@gmail.com');
+insert into students(id,name,age,email) VALUES(4,'Eve',21,'he@gmail.com');
+insert into students(id,name,age,email) VALUES(5,'Charlie',23,'charlie@gmail.com');

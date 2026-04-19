be779caa5910b301b776af94553dc679dbbdb776
unknown <23ai009@sitmng.ac.in>
2026-04-19 21:15:56 +0530

    add the mysql sql for 2.sql

--- day2/faang level questions/2.sql
+++ day2/faang level questions/2.sql
@@ -8,3 +8,8 @@ INSERT INTO employees(id,name,email,salary)
 VALUES(101,'John Doe','rahul@gmail.com',45000)
 --THIS KNOWN AS UPSERT OPERATION WHERE UPDATE AND INSERT WILL TAKES PLACE 
 ON CONFLICT(email) DO NOTHING;
+
+--option 2
+--insert ignore will check the Primary key or Unique Key violations before committing the data.
+INSERT IGNORE INTO employees(id,name,email,salary)
+VALUES(101,'John Doe','rahul@gmail.com',45000);

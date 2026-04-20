d4094a7ebaf328243178f9f1c479b77b661288eb
unknown <23ai009@sitmng.ac.in>
2026-04-20 17:16:06 +0530

    add the doc to understand the concept

--- day2/faang level questions/5.sql
+++ day2/faang level questions/5.sql
@@ -1,4 +1,5 @@
 -- increase the salary of employee who earn below average
+-- THIS QUERY HELP TO INCREASE THE SALARY BY 5000 FOR EMPLOYEES WHO EARN BELOW AVERAGE SALARY.
 UPDATE employees
 SET salary=salary+5000
 WHERE salary < (SELECT AVG(salary) FROM employees);

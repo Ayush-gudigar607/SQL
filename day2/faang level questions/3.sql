d3395f985d9defc3da2016d50067f96da849f98f
unknown <23ai009@sitmng.ac.in>
2026-04-20 17:07:47 +0530

    add the better version of 3.sql

--- day2/faang level questions/3.sql
+++ day2/faang level questions/3.sql
@@ -4,3 +4,10 @@ DELETE FROM orders
 WHERE customer_id NOT IN (
     SELECT id FROM customers
 )
+
+-- better handles null safety
+DELETE FROM orders O
+WHERE NOT EXISTS(
+    SELECT 1 FROM customers c
+    WHERE c.id=o.customer_id
+)

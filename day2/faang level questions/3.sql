72142386919849395471818e9b2e687f08bc7a11
unknown <23ai009@sitmng.ac.in>
2026-04-20 17:08:45 +0530

    Add the document to the command

--- day2/faang level questions/3.sql
+++ day2/faang level questions/3.sql
@@ -6,6 +6,7 @@ WHERE customer_id NOT IN (
 )
 
 -- better handles null safety
+-- o will be represented as orders and c will be represented as customers
 DELETE FROM orders O
 WHERE NOT EXISTS(
     SELECT 1 FROM customers c
